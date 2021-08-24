283af6ae86b109f2616e76c9cad582989cf7bf3f
Your Name <spectraferreyra@gmail.com>
2021-08-24 20:22:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
